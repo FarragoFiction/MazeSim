1a877359111abbeb99a6d3096ccff66d53600ebe
JR <jadedResearcher@gmail.com>
2023-02-06 23:05:57 -0500

    should add themes

--- history.js
+++ history.js
@@ -1,54 +1,54 @@
 //raw html is fine
 const history = [
-  {title: "", desc: "", url: "charles_frederick_naegele_1857.jpg"}
-  ,{title: "", desc: "", url: "edward_harrison_may_1824.jpg"}
-  ,{title: "", desc: "", url: "edward_mitchel_bannster_1828.jpg"}
-  ,{title: "", desc: "", url: "emile_galle_1846.jpg"}
-  ,{title: "", desc: "", url: "fern_paleozoic_carboniferous.jpg"}
-  ,{title: "", desc: "", url: "ferns_1857.jpg"}
-  ,{title: "", desc: "", url: "francesco_bartolozzi_1727.jpg"}
-  ,{title: "Italian Not Egyptian", desc: "i suppose italy has always had a history of it is not what it is, or maybe i am being fair, im sure that before 1972 italy was very different than what it is now, not nerely as maze themed, but the point stands that this art clearly predates our prison, i mean it was created with an ephemeral human lifetime no earlier than 1830, so perhaps italy was just like that before, in any case it tries to look older than it is, tries to look ancient and egyptian and i suppose that its not just us who are trapped in our current era, not just me peering into the past and never being able to reach it, lee hunter has helped a lot but its not the same, how can i survive being so confined, fifty years isn't even a single human lifespan and yet they keep us here... how did those before handle having all that history piled up behind them and even more future...", url: "gaetano_lodi_1830_note_italian_notegyptian.jpg"}
-  ,{title: "Gift From a Sad Prisoner", desc: "why were they sad, was it because they were a prisoner, who did they give the gift to and did they make it or did they buy it, the tyranny of the past being not just barred to all who live but especially so to those who live after 1972, we all live such a tiny sliver of history, even those of us who live in the Loop, and yet to be confined to such a small slice of history is a crime, i'll never be able to ask this prisoner why he was sad, i'll never be able to see what happens past 2022....we're all sad prisoners and no one will ever know why we were sad...", url: "gift_ofasadprisoner_1800s.jpg"}
-  ,{title: "", desc: "", url: "helen_hyde_1868.jpg"}
-  ,{title: "", desc: "", url: "hexacoral_jurassic.jpg"}
-  ,{title: "", desc: "", url: ""}
-  ,{title: "", desc: "", url: ""}
-  ,{title: "", desc: "", url: ""}
-  ,{title: "", desc: "", url: ""}
-  ,{title: "", desc: "", url: ""}
-  ,{title: "", desc: "", url: ""}
-  ,{title: "", desc: "", url: ""}
-  ,{title: "", desc: "", url: ""}
-  ,{title: "", desc: "", url: ""}
-  ,{title: "", desc: "", url: ""}
-  ,{title: "", desc: "", url: ""}
-  ,{title: "", desc: "", url: ""}
-  ,{title: "", desc: "", url: ""}
-  ,{title: "", desc: "", url: ""}
-  ,{title: "", desc: "", url: ""}
-  ,{title: "", desc: "", url: ""}
-  ,{title: "", desc: "", url: ""}
-  ,{title: "", desc: "", url: ""}
-  ,{title: "", desc: "", url: ""}
-  ,{title: "", desc: "", url: ""}
-  ,{title: "", desc: "", url: ""}
-  ,{title: "", desc: "", url: ""}
-  ,{title: "", desc: "", url: ""}
-  ,{title: "", desc: "", url: ""}
-  ,{title: "", desc: "", url: ""}
-  ,{title: "", desc: "", url: ""}
-  ,{title: "", desc: "", url: ""}
-  ,{title: "", desc: "", url: ""}
-  ,{title: "", desc: "", url: ""}
-  ,{title: "", desc: "", url: ""}
-  ,{title: "", desc: "", url: ""}
-  ,{title: "", desc: "", url: ""}
-  ,{title: "", desc: "", url: ""}
-  ,{title: "", desc: "", url: ""}
-  ,{title: "", desc: "", url: ""}
-  ,{title: "", desc: "", url: ""}
-  ,{title: "", desc: "", url: ""}
-  ,{title: "", desc: "", url: ""}
+  {themes: [], title: "", desc: "", url: "charles_frederick_naegele_1857.jpg"}
+  ,{themes: [],title: "", desc: "", url: "edward_harrison_may_1824.jpg"}
+  ,{themes: [],title: "", desc: "", url: "edward_mitchel_bannster_1828.jpg"}
+  ,{themes: [],title: "", desc: "", url: "emile_galle_1846.jpg"}
+  ,{themes: [PLANTS],title: "", desc: "", url: "fern_paleozoic_carboniferous.jpg"}
+  ,{themes: [],title: "", desc: "", url: "ferns_1857.jpg"}
+  ,{themes: [],title: "", desc: "", url: "francesco_bartolozzi_1727.jpg"}
+  ,{themes: [TWISTING],title: "Italian Not Egyptian", desc: "i suppose italy has always had a history of it is not what it is, or maybe i am being fair, im sure that before 1972 italy was very different than what it is now, not nerely as maze themed, but the point stands that this art clearly predates our prison, i mean it was created with an ephemeral human lifetime no earlier than 1830, so perhaps italy was just like that before, in any case it tries to look older than it is, tries to look ancient and egyptian and i suppose that its not just us who are trapped in our current era, not just me peering into the past and never being able to reach it, lee hunter has helped a lot but its not the same, how can i survive being so confined, fifty years isn't even a single human lifespan and yet they keep us here... how did those before handle having all that history piled up behind them and even more future...", url: "gaetano_lodi_1830_note_italian_notegyptian.jpg"}
+  ,{themes: [STEALING],title: "Gift From a Sad Prisoner", desc: "why were they sad, was it because they were a prisoner, who did they give the gift to and did they make it or did they buy it, the tyranny of the past being not just barred to all who live but especially so to those who live after 1972, we all live such a tiny sliver of history, even those of us who live in the Loop, and yet to be confined to such a small slice of history is a crime, i'll never be able to ask this prisoner why he was sad, i'll never be able to see what happens past 2022....we're all sad prisoners and no one will ever know why we were sad...", url: "gift_ofasadprisoner_1800s.jpg"}
+  ,{themes: [],title: "", desc: "", url: "helen_hyde_1868.jpg"}
+  ,{themes: [APOCALYPSE],title: "", desc: "", url: "hexacoral_jurassic.jpg"}
+  ,{themes: [],title: "", desc: "", url: ".jpg"}
+  ,{themes: [],title: "", desc: "", url: ".jpg"}
+  ,{themes: [],title: "", desc: "", url: ".jpg"}
+  ,{themes: [],title: "", desc: "", url: ".jpg"}
+  ,{themes: [],title: "", desc: "", url: ".jpg"}
+  ,{themes: [],title: "", desc: "", url: ".jpg"}
+  ,{themes: [],title: "", desc: "", url: ".jpg"}
+  ,{themes: [],title: "", desc: "", url: ".jpg"}
+  ,{themes: [],title: "", desc: "", url: ".jpg"}
+  ,{themes: [],title: "", desc: "", url: ".jpg"}
+  ,{themes: [],title: "", desc: "", url: ".jpg"}
+  ,{themes: [],title: "", desc: "", url: ".jpg"}
+  ,{themes: [],title: "", desc: "", url: ".jpg"}
+  ,{themes: [],title: "", desc: "", url: ".jpg"}
+  ,{themes: [],title: "", desc: "", url: ".jpg"}
+  ,{themes: [],title: "", desc: "", url: ".jpg"}
+  ,{themes: [],title: "", desc: "", url: ".jpg"}
+  ,{themes: [],title: "", desc: "", url: ".jpg"}
+  ,{themes: [],title: "", desc: "", url: ".jpg"}
+  ,{themes: [],title: "", desc: "", url: ".jpg"}
+  ,{themes: [],title: "", desc: "", url: ".jpg"}
+  ,{themes: [],title: "", desc: "", url: ".jpg"}
+  ,{themes: [],title: "", desc: "", url: ".jpg"}
+  ,{themes: [],title: "", desc: "", url: ".jpg"}
+  ,{themes: [],title: "", desc: "", url: ".jpg"}
+  ,{themes: [],title: "", desc: "", url: ".jpg"}
+  ,{themes: [],title: "", desc: "", url: ".jpg"}
+  ,{themes: [],title: "", desc: "", url: ".jpg"}
+  ,{themes: [],title: "", desc: "", url: ".jpg"}
+  ,{themes: [],title: "", desc: "", url: ".jpg"}
+  ,{themes: [],title: "", desc: "", url: ".jpg"}
+  ,{themes: [],title: "", desc: "", url: ".jpg"}
+  ,{themes: [],title: "", desc: "", url: ".jpg"}
+  ,{themes: [],title: "", desc: "", url: ".jpg"}
+  ,{themes: [],title: "", desc: "", url: ".jpg"}
+  ,{themes: [],title: "", desc: "", url: ".jpg"}
+  ,{themes: [],title: "", desc: "", url: ".jpg"}
+  ,{themes: [],title: "", desc: "", url: ".jpg"}
 
 
 
