4b89551fdc51bddaeb820cbe7d5b28e350f80426
JR <jadedResearcher@gmail.com>
2023-02-09 20:43:45 -0500

    finished rivers placeholders

--- history.js
+++ history.js
@@ -1,7 +1,7 @@
 //raw html is fine
 const history = [
-  {themes: [], title: "Mother and Child", desc: "it's surprising the care mothers have for their children, or i guess it's not, as i am both mother and daughter... that doesn't make it easier to parse, or easier to look through my memories for it, or understandable from this woman's perspective... was it scary to have a child back then, i wonder, was it the same feeling all creatures feel when reproducing-- either way it has to be somewhat cruel... countless children died before i could ever see them, and so many don't get to live their lives before the apocalypse... just a speck in the wind, tiny, worthless, inconsequential... i wonder what it's like to live in the would forever...", url: "charles_frederick_naegele_1857.jpg"}
-  ,{themes: [],title: "", desc: "", url: "edward_harrison_may_1824.jpg"}
+  {themes: [], title: "Mother and Child: 1857", desc: "it's surprising the care mothers have for their children, or i guess it's not, as i am both mother and daughter... that doesn't make it easier to parse, or easier to look through my memories for it, or understandable from this woman's perspective... was it scary to have a child back then, i wonder, was it the same feeling all creatures feel when reproducing-- either way it has to be somewhat cruel... countless children died before i could ever see them, and so many don't get to live their lives before the apocalypse... just a speck in the wind, tiny, worthless, inconsequential... i wonder what it's like to live in the would forever...", url: "charles_frederick_naegele_1857.jpg"}
+  ,{themes: [],title: "Sad Woman 1824", desc: "1824... she looks so morose... sad... i wonder what she is thinking about, in that state of hers-- i don't have enough data on this year, or any year in that bracket, to approximate the reasons why she would be feeling this way... i need to remind myself to not get angry at the people in this world for not keeping their records... they are so small, so unencompassing, it would not be possible, it would not be feasible, so this is all i have... i reserve the right to complain, though...", url: "edward_harrison_may_1824.jpg"}
   ,{themes: [],title: "", desc: "", url: "edward_mitchel_bannster_1828.jpg"}
   ,{themes: [],title: "", desc: "", url: "emile_galle_1846.jpg"}
   ,{themes: [PLANTS],title: "", desc: "", url: "fern_paleozoic_carboniferous.jpg"}
@@ -11,46 +11,21 @@ const history = [
   ,{themes: [STEALING],title: "Gift From a Sad Prisoner", desc: "why were they sad, was it because they were a prisoner, who did they give the gift to and did they make it or did they buy it, the tyranny of the past being not just barred to all who live but especially so to those who live after 1972, we all live such a tiny sliver of history, even those of us who live in the Loop, and yet to be confined to such a small slice of history is a crime, i'll never be able to ask this prisoner why he was sad, i'll never be able to see what happens past 2022....we're all sad prisoners and no one will ever know why we were sad...", url: "gift_ofasadprisoner_1800s.jpg"}
   ,{themes: [],title: "", desc: "", url: "helen_hyde_1868.jpg"}
   ,{themes: [APOCALYPSE],title: "", desc: "", url: "hexacoral_jurassic.jpg"}
-  ,{themes: [],title: "", desc: "", url: ".jpg"}
-  ,{themes: [],title: "", desc: "", url: ".jpg"}
-  ,{themes: [],title: "", desc: "", url: ".jpg"}
-  ,{themes: [],title: "", desc: "", url: ".jpg"}
-  ,{themes: [],title: "", desc: "", url: ".jpg"}
-  ,{themes: [],title: "", desc: "", url: ".jpg"}
-  ,{themes: [],title: "", desc: "", url: ".jpg"}
-  ,{themes: [],title: "", desc: "", url: ".jpg"}
-  ,{themes: [],title: "", desc: "", url: ".jpg"}
-  ,{themes: [],title: "", desc: "", url: ".jpg"}
-  ,{themes: [],title: "", desc: "", url: ".jpg"}
-  ,{themes: [],title: "", desc: "", url: ".jpg"}
-  ,{themes: [],title: "", desc: "", url: ".jpg"}
-  ,{themes: [],title: "", desc: "", url: ".jpg"}
-  ,{themes: [],title: "", desc: "", url: ".jpg"}
-  ,{themes: [],title: "", desc: "", url: ".jpg"}
-  ,{themes: [],title: "", desc: "", url: ".jpg"}
-  ,{themes: [],title: "", desc: "", url: ".jpg"}
-  ,{themes: [],title: "", desc: "", url: ".jpg"}
-  ,{themes: [],title: "", desc: "", url: ".jpg"}
-  ,{themes: [],title: "", desc: "", url: ".jpg"}
-  ,{themes: [],title: "", desc: "", url: ".jpg"}
-  ,{themes: [],title: "", desc: "", url: ".jpg"}
-  ,{themes: [],title: "", desc: "", url: ".jpg"}
-  ,{themes: [],title: "", desc: "", url: ".jpg"}
-  ,{themes: [],title: "", desc: "", url: ".jpg"}
-  ,{themes: [],title: "", desc: "", url: ".jpg"}
-  ,{themes: [],title: "", desc: "", url: ".jpg"}
-  ,{themes: [],title: "", desc: "", url: ".jpg"}
-  ,{themes: [],title: "", desc: "", url: ".jpg"}
-  ,{themes: [],title: "", desc: "", url: ".jpg"}
-  ,{themes: [],title: "", desc: "", url: ".jpg"}
-  ,{themes: [],title: "", desc: "", url: ".jpg"}
-  ,{themes: [],title: "", desc: "", url: ".jpg"}
-  ,{themes: [],title: "", desc: "", url: ".jpg"}
-  ,{themes: [],title: "", desc: "", url: ".jpg"}
-  ,{themes: [],title: "", desc: "", url: ".jpg"}
-  ,{themes: [],title: "", desc: "", url: ".jpg"}
-
-
-
-
+  ,{themes: [],title: "", desc: "", url: "jose_maria_mora_1847.jpg"}
+  ,{themes: [],title: "", desc: "", url: "joseph_mozier_1812.jpg"}
+  ,{themes: [],title: "", desc: "", url: "joseph_mozier_1812_melancholy.jpg"}
+  ,{themes: [],title: "", desc: "", url: "kenyon_cox_1856_notenotancient.jpg"}
+  ,{themes: [],title: "", desc: "", url: "kudelski_group_1951.jpg"}
+  ,{themes: [],title: "", desc: "", url: "maze_motif_1800s.jpg"}
+  ,{themes: [],title: "", desc: "", url: "miner_kilbourne_kellog_1814.jpg"}
+  ,{themes: [],title: "", desc: "", url: "museum_mineral_exhibit_1959.jpg"}
+  ,{themes: [],title: "", desc: "", url: "night_thoughts_on_life_death_and_immortality_volumeII_by_edward_young_1816.jpg"}
+  ,{themes: [],title: "", desc: "", url: "pan_american_world_airways_system_1931.jpg"}
+  ,{themes: [DOLLS,CLOWNS],title: "Pulcinella", desc: "", url: "pulcinella_1700s.jpg"}
+  ,{themes: [],title: "", desc: "", url: "rip_van_winkle_1839.jpg"}
+  ,{themes: [],title: "Sad Iron", desc: "why is the iron sad, did it know it would outlive everyone who ever used it, who ever loved it, who ever relied on it--did it become sad after it became rusty and forgotten or was it born sad, did it ever know a time without sadness...will i", url: "sad_iron_1860.jpg"}
+  ,{themes: [],title: "", desc: "", url: "snail_case_whiting_manufacturing_company_1866.jpg"}
+  ,{themes: [],title: "", desc: "", url: "snail_paleozoic_carboniferous.jpg"}
+  ,{themes: [],title: "", desc: "", url: "thomascrawford_1813.jpg"}
+  ,{themes: [],title: "", desc: "", url: "unidentified_roman.jpg"}
 ];
